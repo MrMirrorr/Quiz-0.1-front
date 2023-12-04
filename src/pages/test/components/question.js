8edb9dee98f4b8bffd24415fa058e5c9b30f13df
AndyR8219 <andreymail1982@gmail.com>
2023-12-04 20:19:35 +0300

    корректировка

--- src/pages/test/components/question.js
+++ src/pages/test/components/question.js
@@ -1,23 +1,51 @@
 import { Form } from 'react-bootstrap';
 
-export const Question = ({ data, questionsNum }) => {
+export const Question = ({
+	data,
+	questionNumber,
+	value,
+	setValue,
+	setIsChecked,
+	setTesting,
+	questionsLength,
+	testing,
+}) => {
+	const questionTitle = data.questions[questionNumber].title;
+	const questionAnswers = data.questions[questionNumber].answers;
+
+	function chengeValue(event) {
+		setValue(event.target.value);
+		setIsChecked(true);
+		setTesting((prev) => {
+			return {
+				...prev,
+				[questionTitle]: event.target.value,
+			};
+		});
+		console.log(testing);
+	}
+
 	return (
 		<>
 			<div className="mb-1">
-				Вопрос {questionsNum + 1} из {data.questions.length}
+				Вопрос {questionNumber + 1} из {questionsLength}
 			</div>
-			<div className="mb-5 fs-3 fw-bold">{data.questions[questionsNum].title}</div>
+			<div className="mb-5 fs-3 fw-bold">{questionTitle}</div>
 			<Form
 				className="d-flex flex-column align-items-start m-auto"
 				style={{ maxWidth: 400 }}
 			>
-				{data.questions[questionsNum].answers.map((answer) => (
+				{questionAnswers.map((answer, index) => (
 					<Form.Check
 						key={answer.title}
-						className="mb-3"
+						className=" mb-3"
 						type="radio"
-						name="answer"
+						name="flexRadioDefault"
+						id={`flexRadioDefault${index}`}
 						label={answer.title}
+						value={answer.title}
+						checked={value === answer.title ? true : false}
+						onChange={(e) => chengeValue(e)}
 					/>
 				))}
 			</Form>
--- src/pages/test/test.js
+++ src/pages/test/test.js
@@ -1,10 +1,15 @@
 import { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { Question } from './components/question';
+import { useNavigate } from 'react-router-dom';
 
 export const Test = () => {
 	const [data, setData] = useState();
+	const [value, setValue] = useState();
+	const [testing, setTesting] = useState({});
+	const [isChecked, setIsChecked] = useState(false);
 	const [questionNumber, setQuestionNumber] = useState(0);
+	const navigate = useNavigate();
 
 	useEffect(() => {
 		fetch('/api/')
@@ -26,36 +31,52 @@ export const Test = () => {
 		return <h1>Loading...</h1>;
 	}
 
+	const questionsLength = data.questions.length;
+
 	const previousQuestion = () => {
-		console.log('Previous');
 		setQuestionNumber((prev) => prev - 1);
+		setIsChecked(false);
 	};
 
 	const nextQuestion = () => {
 		setQuestionNumber((prev) => prev + 1);
+		setIsChecked(false);
+		console.log(testing);
 	};
 
 	const completeTest = () => {
+		localStorage.setItem(`${Date.now()}`, JSON.stringify(testing));
+		navigate('/');
 		console.log('Тест завершен');
 	};
 
-	const lastQuestion = !(data.questions.length === questionNumber + 1) ? true : false;
+	const lastQuestion = !(questionsLength === questionNumber + 1) ? true : false;
 
 	return (
 		<div className="text-center">
 			<h1 className="mb-5">Прохождение теста</h1>
-			<Question data={data} questionsNum={questionNumber} />
+			<Question
+				data={data}
+				questionNumber={questionNumber}
+				value={value}
+				setValue={setValue}
+				setIsChecked={setIsChecked}
+				setTesting={setTesting}
+				testing={testing}
+				questionsLength={questionsLength}
+			/>
 			<div className="my-5">
 				<Button
 					className="btn btn-primary "
+					disabled={!questionNumber || !isChecked ? true : false}
 					onClick={() => previousQuestion()}
-					disabled={!questionNumber ? true : false}
 				>
 					Предыдущий вопрос
 				</Button>
 				{lastQuestion ? (
 					<Button
 						className="btn btn-primary nextBtn mx-3"
+						disabled={!isChecked}
 						onClick={() => nextQuestion()}
 					>
 						Следующий вопрос
@@ -63,6 +84,7 @@ export const Test = () => {
 				) : (
 					<Button
 						className="btn btn-primary mx-3"
+						disabled={!isChecked}
 						onClick={() => completeTest()}
 					>
 						Завершить тест
